30ac3f762167e84303df5b28865881a8c884e2fe
queukat <75810528+queukat@users.noreply.github.com>
2026-04-28 06:26:41 +0400

    Bump app version to 1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.queukat.sbsgeorgia"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
